f02d14e2866ab94fa2e8d2286a7f5d4015e893db
PavelSeroglazlin <mr.seroglazkin@bk.ru>
2022-11-14 15:22:30 +0400

    Исправлены файлы: Father.java, Mother.java

--- practice_4/practice_4/Father.java
+++ practice_4/practice_4/Father.java
@@ -1,22 +1,11 @@
 package practice_4;
 
-//import java.util.ArrayList;
-import java.util.List;
-
 public class Father extends Person {
-    // private List<Person> children;
+    private String gender = "male";
 
     public Father(String surname, String name, String patronymic, Person parent1, Person parent2, int age) {
         super(surname, name, patronymic, parent1, parent2, age);
-        // children = new ArrayList<>();
-    }
 
-    public List<Person> getChildren() {
-        return children;
-    }
-
-    public void setChildren(List<Person> children) {
-        this.children = children;
     }
 
     public void add(Person person) {
@@ -28,6 +17,10 @@ public class Father extends Person {
         return "Father [surname=" + surname + ", name=" + name + ", patronymic=" + patronymic + ", parent1=" + parent1
                 + ", parent2=" + parent2 + ", age=" + age + ", children=" + children
                 + "]";
+    }
+
+    public String getGender() {
+        return gender;
     }
 
 }
--- practice_4/practice_4/Mother.java
+++ practice_4/practice_4/Mother.java
@@ -1,26 +1,16 @@
 package practice_4;
 
-//import java.util.ArrayList;
-import java.util.List;
+//import java.util.List;
 
 public class Mother extends Person {
-    // private List<Person> children;
+    private String gender = "female";
 
     public Mother(String surname, String name, String patronymic, Person parent1, Person parent2, int age) {
         super(surname, name, patronymic, parent1, parent2, age);
-        // children = new ArrayList<>();
     }
 
     public void add(Person person) {
         children.add(person);
-    }
-
-    public List<Person> getChildren() {
-        return children;
-    }
-
-    public void setChildren(List<Person> children) {
-        this.children = children;
     }
 
     @Override
@@ -28,5 +18,9 @@ public class Mother extends Person {
         return "Mother [surname=" + surname + ", name=" + name + ", patronymic=" + patronymic + ", age=" + age
                 + ", children=" + children
                 + "]";
+    }
+
+    public String getGender() {
+        return gender;
     }
 }
